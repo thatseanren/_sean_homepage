2f4513fdfcb9e59e73701b4cbba592241982a7b4
seanren96 <seanren96@gmail.com>
2019-11-28 09:51:23 +1100

    f

---
+++ src/component/avatar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import ren from '../ren.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '100px',
+        width: '100px',
+        top: '35px',
+    },
+}));
+
+export default function Avatar_ren() {
+    const classes = useStyles();
+
+    return (<Avatar alt='sean ren' src={ren} className={clsx(classes.root, 'centered')}>
+        </Avatar>
+    );
+}
