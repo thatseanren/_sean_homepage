2cdaade0a2840ebcd74bf6afe4debd7394b1fef9
seanren96 <seanren96@gmail.com>
2019-11-28 22:31:54 +1100

    add btn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Avatar from './component/avatar'
+import MenuBtn from './component/menubtn'
 ReactDOM.render(<Avatar />, document.getElementsByClassName('component-avatar')[0]);
-
+ReactDOM.render(<MenuBtn />, document.getElementById('btn'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
