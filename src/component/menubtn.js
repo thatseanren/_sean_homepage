c8a401f2b39a2bfb34ea0e9b3aa0ed4b5f668604
seanren96 <seanren96@gmail.com>
2019-11-29 00:15:10 +1100

    add navi
    Signed-off-by: seanren96 <seanren96@gmail.com>

--- src/component/menubtn.js
+++ src/component/menubtn.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-import'./menubtn.css';
+import './menubtn.css';
 import $ from 'jquery';
 export default function MenuBtn() {
     useEffect(() => {
-        $('.menu-btn').on('click',()=>{$('.menu-btn').hasClass('show') ? $('.menu-btn').removeClass('show') :$('.menu-btn').addClass('show') })
-      }, []);
+        $('.menu-btn').on('click',
+            () => {
+                $('.menu-btn').hasClass('show') ?
+                    ($('.menu-btn').removeClass('show') && $('.h-section-navi').removeClass('show') && $('.MuiAvatar-root').removeClass('show')) : ($('.menu-btn').addClass('show') && $('.h-section-navi').addClass('show') && $('.MuiAvatar-root').addClass('show'))
+            })
+    }, []);
     return (<a class='menu-btn'>
         <div class='line line1'></div>
         <div class='line line2'></div>
