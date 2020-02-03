86a60e15d29fdc5d34501fd12a873beb2fbc2f5d
seanren96 <seanren96@gmail.com>
2020-02-03 12:36:29 +0800

    adding console.log()

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Avatar from './component/avatar'
 import MenuBtn from './component/menubtn'
 // cons3ole.log(this)//undefined
 console.log(Avatar)
+console.log('fdsa')
 ReactDOM.render(<Avatar />, document.getElementsByClassName('component-avatar')[0]);
 ReactDOM.render(<MenuBtn />, document.getElementById('btn'));
 // If you want your app to work offline and load faster, you can change
